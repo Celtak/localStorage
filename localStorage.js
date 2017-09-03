2107cc26895fa1073e0b699aacfde570762ec332
Henrique <rtrans.web@gmail.com>
2017-09-03 17:51:29 +0200

    Create Storage

--- localStorage.js
+++ localStorage.js
@@ -1,1 +1,36 @@
-if(localc)
+function Storage(nameStorage, init) {
+
+    this.nameStorage = nameStorage;
+
+    if (localStorage.getItem(nameStorage) === null) {
+
+        localStorage.setItem(nameStorage, init);
+
+        a = nameStorage;
+
+        console.warn('Initialization: ' + nameStorage + ' with ' + init);
+
+
+    } else {
+
+        console.warn('No Initialization because ' + nameStorage + ' is already used!');
+
+    }
+
+    this.load = function () {
+
+        return localStorage.getItem(this.nameStorage);
+
+    }
+
+    this.update = function (data) {
+
+
+        localStorage.setItem(nameStorage, data);
+
+        console.warn('Update: ' + nameStorage);
+
+
+    }
+
+}
